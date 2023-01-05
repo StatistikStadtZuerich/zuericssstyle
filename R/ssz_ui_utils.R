9204a77734455323452f072bb42945de10e9a9d1
Hänzi Sara (SSZ) <Sara.Haenzi@zuerich.ch>
2023-01-05 11:42:55 +0100

    adjust autocomplete function with new class, small css corrections

--- R/ssz_ui_utils.R
+++ R/ssz_ui_utils.R
@@ -199,6 +199,7 @@ sszTextInput <- function(inputId, label, ...) {
 #'
 #' @param inputId inputId parameter for autocomplete_input
 #' @param label label parameter for autocomplete_input
+#' @param options options parameter for autocomplete_input
 #' @param ... further parameters for autocomplete_input
 #'
 #' @return same as autocomplete_input
@@ -212,8 +213,10 @@ sszTextInput <- function(inputId, label, ...) {
 #'  unique(addresses$StrasseLang)
 #' )
 #' }
-sszAutocompleteInput <- function(inputId, label, ...) {
-  dqshiny::autocomplete_input(inputId, label, ...)
+sszAutocompleteInput <- function(inputId, label, options, ...) {
+  html_list <- dqshiny::autocomplete_input(inputId, label, options, ...)
+  html_list$children[[1]]$attribs$class <- "ssz-label"
+  html_list
 }
 
 
