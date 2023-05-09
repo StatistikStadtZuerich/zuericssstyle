e343a3d4dff7b3773569ed7c0020ea6411118aef
sszmea <aline.metzler@zuerich.ch>
2023-05-09 15:53:02 +0200

    add function for sszSliderInput

--- R/ssz_ui_utils.R
+++ R/ssz_ui_utils.R
@@ -256,6 +256,41 @@ sszNumericInput <- function(inputId, label, value, ...) {
 }
 
 
+#' sszSliderInput
+#'
+#' @description function to generate a sliderInput with the custom ssz label styling (via "ssz-label" class)
+#'
+#' @param inputId inputId parameter for sliderInput
+#' @param label label parameter for sliderInput
+#' @param value value parameter (initial value) for sliderInput
+#' @param min min value that can be selected for sliderInput
+#' @param max max value that can be selected for sliderInput
+#' @param ... further named parameters to be passed to sliderInput (step/ticks/width)
+#'
+#' @return the same as textInput with modified label class
+#' @export
+#'
+#' @examples
+#' sszSliderInput(inputId = "grade_number", label = "Grade:", value = 5, min = 1, max = 6)
+sszSliderInput <- function(inputId, label, value, min, max, ...) {
+  html_list <- sliderInput(
+    inputId = inputId,
+    label = label,
+    value = value,
+    min = min,
+    max = max,
+    ...
+  )
+
+  current_label_class <- html_list$children[[1]]$attribs$class
+  html_list$children[[1]]$attribs$class <- paste(
+    current_label_class,
+    "sszRadioButton-label"
+  )
+  html_list
+}
+
+
 #' sszRadioButtons
 #'
 #' @description function to generate radioButtons with the custom ssz (label) styling
