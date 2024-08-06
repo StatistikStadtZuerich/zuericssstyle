08527138d050ca529f95c3dba7721544140ae2ed
Feller Alessandro (SSZ) <Alessandro.Feller@zuerich.ch>
2024-08-06 17:35:24 +0200

    Update description

--- R/zuericssstyle-package.R
+++ R/zuericssstyle-package.R
@@ -1,5 +1,7 @@
+#' @import here
 #' @importFrom htmltools tags
-#' @importFrom shiny dateRangeInput selectInput textInput radioButtons actionButton numericInput downloadButton
+#' @import icons
+#' @importFrom shiny dateRangeInput selectInput textInput radioButtons actionButton numericInput downloadButton sliderInput
 #' @importFrom shinyWidgets airDatepickerInput
 #' @importFrom dqshiny autocomplete_input
 NULL
