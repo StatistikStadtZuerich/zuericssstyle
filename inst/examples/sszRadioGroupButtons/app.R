413f8aeec9d95a59c3b6ef5e9336a347c02c0028
Hänzi Sara (SSZ) <Sara.Haenzi@zuerich.ch>
2024-10-28 17:02:40 +0100

    add other buttons to test app to make sure these are still ok

--- inst/examples/sszRadioGroupButtons/app.R
+++ inst/examples/sszRadioGroupButtons/app.R
@@ -1,5 +1,6 @@
 library(shiny)
 # radio group button demo (from shinyWidgets)
+icons_ssz <- icons::icon_set(here::here("inst", "examples", "icons"))
 
 ui <- ssz_page(
   includeCSS("test.css"),
@@ -9,7 +10,26 @@ ui <- ssz_page(
     choices = 2022:2024,
     selected = 2022 # default value
   ),
-  #actionButton("bla", "bla"),
+
+  # add buttons to check our buttons are still ok
+  sszActionButton(
+    "action",
+    "Abfrage starten",
+  ),
+  tags$div(
+    id = "downloadWrapperId",
+    class = "downloadWrapperDiv",
+    sszDownloadButton("csvDownload",
+                      label = "csv",
+                      image = img(icons_ssz("download"))
+    ),
+    sszOgdDownload(
+      outputId = "ogdDown",
+      label = "OGD",
+      href = "https://data.stadt-zuerich.ch/",
+      image = img(icons_ssz("external-link"))
+    )
+  ),
   br(),
   h1("Selected"),
   textOutput("choice")
