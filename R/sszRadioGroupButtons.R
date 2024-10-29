15e4a6d7e1f9755fae565ad00ef65288ed4e796e
Hänzi Sara (SSZ) <Sara.Haenzi@zuerich.ch>
2024-10-29 09:25:03 +0100

    extend function documentation, remove examples that were only for development

--- R/sszRadioGroupButtons.R
+++ R/sszRadioGroupButtons.R
@@ -1,18 +1,18 @@
 #' sszRadioGroupButtons
 #'
-#' @description function to generate radioButtons with the custom ssz (label) styling
+#' @description function to generate radioGroupButtons from shinyWidgets with the custom ssz (label) styling
 #'
-#' @details a "sszRadioButton" is added to the output of radioButtons, and a
-#'  "sszRadioButton-label" class is added to the appropriate child. the whole thing is
-#'  wrapped in a div with class "radioDiv"
+#' @details classes ssz-radiogroup-1, ssz-radiogroup-2, and ssz-radiogroup-3 are added
+#' (starting from outermost div, moving down 1 step at a time); though currently these are not used for styling;
+#' only .btn-group-toggle>.btn-default and .btn-default.active are used for the styling
 #'
-#' @param inputId inputId parameter for radioButtons
-#' @param label label parameter for radioButtons, default NULL; leave null for button above chart
-#' @param choices choices parameter for radioButtons, default NULL
+#' @param inputId inputId parameter for radioGroupButtons
+#' @param label label parameter for radioGroupButtons, default NULL; leave NULL for button above chart (i.e. no label)
+#' @param choices choices parameter for radioGroupButtons, default NULL
 #' @param selected initially selected value, default NULL
 #' @param ... further named parameters for shinyWidget::radioGroupButtons
 #'
-#' @return the return of radioGroupButtons with modified labels
+#' @return the return of radioGroupButtons with modified html classes
 #'
 #' @export
 #'
@@ -28,29 +28,17 @@ sszRadioGroupButtons <- function(inputId, label = NULL, choices = NULL, selected
     ...
   )
 
+  # add ssz-radiogroup-1 class to outermost div
   current_class_1 <- html_list$attribs$class
-  # add ssz-radiogroup class
   html_list$attribs$class <- paste(current_class_1, "ssz-radiogroup-1")
 
+  # add ssz-radiogroup-2 class to div one layer down
   current_class_2 <- html_list$children[[3]]$attribs$class
   html_list$children[[3]]$attribs$class <- paste(current_class_2, "ssz-radiogroup-2")
 
+  # add ssz-radiogroup-3 class to div another layer down
   current_class_3 <- html_list$children[[3]]$children[[1]]$attribs$class
   html_list$children[[3]]$children[[1]]$attribs$class <- paste(current_class_3, "ssz-radiogroup-3")
 
-
   html_list
-
-
-  # htmltools::tagQuery(a)$find("something...")
-  #
-  # individual buttons
-  # current_class <- html_list$attribs$class
-  # html_list$attribs$class <- paste(current_class, "sszRadioButton")
-  #
-  # current_label_class <- html_list$children[[1]]$attribs$class
-  # html_list$children[[1]]$attribs$class <- paste(
-  #   current_label_class,
-  #   "sszRadioButton-label"
-  # )
 }
--- inst/examples/sszRadioGroupButtons/app.R
+++ inst/examples/sszRadioGroupButtons/app.R
@@ -1,6 +1,5 @@
 library(shiny)
 # radio group button demo (from shinyWidgets)
-icons_ssz <- icons::icon_set(here::here("inst", "examples", "icons"))
 
 ui <- ssz_page(
   includeCSS("test.css"),
@@ -9,26 +8,6 @@ ui <- ssz_page(
     inputId = "years",
     choices = 2022:2024,
     selected = 2022 # default value
-  ),
-
-  # add buttons to check our buttons are still ok
-  sszActionButton(
-    "action",
-    "Abfrage starten",
-  ),
-  tags$div(
-    id = "downloadWrapperId",
-    class = "downloadWrapperDiv",
-    sszDownloadButton("csvDownload",
-                      label = "csv",
-                      image = img(icons_ssz("download"))
-    ),
-    sszOgdDownload(
-      outputId = "ogdDown",
-      label = "OGD",
-      href = "https://data.stadt-zuerich.ch/",
-      image = img(icons_ssz("external-link"))
-    )
   ),
   br(),
   h1("Selected"),
