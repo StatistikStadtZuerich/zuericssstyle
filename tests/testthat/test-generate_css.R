67a43d143b0c2a0fac85019694d48cdada78c863
Sara Hänzi (sszhas) <sara.haenzi@zuerich.ch>
2025-04-01 09:38:59 +0200

    add tests on generating css files

---
+++ tests/testthat/test-generate_css.R
@@ -0,0 +1,34 @@
+test_that("css files can be generated based on scss", {
+
+  # print(here())
+  # shiny css
+  temp_file <- "temp.css"
+  expect_no_error(
+    # css for shiny apps including all widgets
+    sass::sass(
+      sass::sass_file(here("scss", "main_shiny.scss")),
+      temp_file,
+      options = sass::sass_options(
+        output_style = "compressed",
+        source_map_embed = TRUE
+      )
+    )
+  )
+  expect_true(file.exists(temp_file))
+  file.remove(temp_file)
+
+  # general css
+  temp_file <- "temp.css"
+  expect_no_error(
+    # css for shiny apps including all widgets
+    sass::sass(
+      sass::sass_file(here("scss", "main.scss")),
+      temp_file,
+      options = sass::sass_options(
+        source_map_embed = TRUE
+      )
+    )
+  )
+  expect_true(file.exists(temp_file))
+  file.remove(temp_file)
+})
