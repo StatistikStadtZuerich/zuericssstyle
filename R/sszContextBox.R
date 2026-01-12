444f9690d15da4f1f49ae56bb665bfe372b5b8d1
Feller Alessandro (SSZ) <Alessandro.Feller@zuerich.ch>
2026-01-12 19:29:27 +0100

    sszContextBox rename function

--- R/sszContextBox.R
+++ R/sszContextBox.R
@@ -3,7 +3,7 @@
 #' @description
 #' Function to generate a styled info box with optional title, text, and icon.
 #' The layout uses a flex container for the icon and text, and applies
-#' package-specific classes ("info-div", "info-text", "sszContextBox") for consistent styling.
+#' package-specific classes ("context-box-div", "context-box-text", "sszContextBox") for consistent styling.
 #'
 #' @param title A character string for the heading of the context box (displayed as h6).
 #' @param text A character string for the body text of the context box.
@@ -23,18 +23,20 @@
 #' }
 sszContextBox <- function(title, text, icon = NULL) {
   # Outer container
-  box_div <- tags$div(
-    class = "info-div",
+  html_list <- tags$div(
+    class = "context-box-div",
 
     # Header bar with h2 title
-    if (!is.null(title)) tags$div(
-      class = "header-bar",
-      h2(title)
-    ),
+    if (!is.null(title)) {
+      tags$div(
+        class = "context-box-header-bar",
+        h2(title)
+      )
+    },
 
     # Content box with icon + text
     tags$div(
-      class = "content-box info-text",
+      class = "content-box context-box-text",
 
       # Icon (optional)
       if (!is.null(icon)) icon,
@@ -45,8 +47,8 @@ sszContextBox <- function(title, text, icon = NULL) {
   )
 
   # Add a custom class for easy targeting if needed
-  current_class <- box_div$attribs$class
-  box_div$attribs$class <- paste(current_class, "sszContextBox")
+  current_class <- html_list$attribs$class
+  html_list$attribs$class <- paste(current_class, "sszContextBox")
 
-  box_div
+  html_list
 }
