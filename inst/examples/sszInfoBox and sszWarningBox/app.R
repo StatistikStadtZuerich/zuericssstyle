81c0d5a3bd3b8b0b4a76908445c657125c379385
Feller Alessandro (SSZ) <Alessandro.Feller@zuerich.ch>
2026-01-13 17:45:15 +0100

    demo widget for sszInfoBox and sszWarningBox

---
+++ inst/examples/sszInfoBox and sszWarningBox/app.R
@@ -0,0 +1,23 @@
+library(shiny)
+# action button demo
+
+icons_ssz <- icons::icon_set(here::here("inst", "examples", "icons"))
+
+ui <- ssz_page(
+  sszInfoBox(
+    title = "A message with the wrong sender will not be answered and will be deleted.",
+    text = "Laboris laborum aute id laboris culpa esse aliquip nisi anim velit. Minim sunt eiusmod do laborum amet ut magna. Labore dolore id nostrud enim Lorem pariatur ad dolore id eiusmod adipisicing laboris laborum minim.",
+    icon = icons_ssz("info-help-filled")
+  ),
+  sszWarningBox(
+    title = "Title Error",
+    text = "Laboris laborum aute id laboris culpa esse aliquip nisi anim velit. Minim sunt eiusmod do laborum amet ut magna. Labore dolore id nostrud enim Lorem pariatur ad dolore id eiusmod adipisicing laboris laborum minim.",
+    icon = icons_ssz("important-warning-filled")
+  )
+)
+
+server <- function(input, output, session) {
+
+}
+
+shinyApp(ui, server)
