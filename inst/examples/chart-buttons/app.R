d00c988f3f160318409fc7e00ea0c0c59c0c7021
Feller Alessandro (SSZ) <Alessandro.Feller@zuerich.ch>
2026-03-18 17:22:25 +0100

    get rid of main panel

--- inst/examples/chart-buttons/app.R
+++ inst/examples/chart-buttons/app.R
@@ -2,18 +2,12 @@ library(shiny)
 # button chart div demo
 
 ui <- ssz_page(
-  h1("Demo app: button for chart"),
-  sidebarPanel(
-    h3("something in the sidebar")
-  ),
-  mainPanel(
-    h3("and something else in the main panel"),
-    div(
-      class = "ssz-chart-buttons",
-      sszRadioGroupButtons(
-        inputId = "choice_year",
-        choices = c(2022, 2023, 2024)
-      )
+  h3("and something else in the main panel"),
+  div(
+    class = "ssz-chart-buttons",
+    sszRadioGroupButtons(
+      inputId = "choice_year",
+      choices = c(2022, 2023, 2024)
     )
   )
 )
