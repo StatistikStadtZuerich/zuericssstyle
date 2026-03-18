58dfc0227a882a056bc2f2c72f3f0a9cc8bf9504
Feller Alessandro (SSZ) <Alessandro.Feller@zuerich.ch>
2026-03-18 18:24:45 +0100

    reactable styling

--- inst/examples/reactable/app.R
+++ inst/examples/reactable/app.R
@@ -4,13 +4,30 @@ library(reactable)
 
 ui <- ssz_page(
   h1("Widget demo"),
-  reactable(iris,
-    paginationType = "simple",
-    language = reactableLang(
-      noData = "Keine Einträge gefunden",
-      pageNumbers = "{page} von {pages}",
-      pagePrevious = "\u276e",
-      pageNext = "\u276f"
+  tagList(
+    h2("Statische Tabelle"),
+    reactable::reactable(
+      iris,
+      paginationType = "simple",
+      class = "table-striped",
+      language = reactable::reactableLang(
+        noData = "Keine Einträge gefunden",
+        pageNumbers = "{page} von {pages}",
+        pagePrevious = "\u276e",
+        pageNext = "\u276f"
+      )
+    ),
+    h2("Interaktive Tabelle"),
+    reactable::reactable(
+      iris,
+      paginationType = "simple",
+      class = "table-hover",
+      language = reactable::reactableLang(
+        noData = "Keine Einträge gefunden",
+        pageNumbers = "{page} von {pages}",
+        pagePrevious = "\u276e",
+        pageNext = "\u276f"
+      )
     )
   )
 )
