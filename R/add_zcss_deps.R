53af777ba14f78483511cc9d2c30c82fb0e5ed6e
Feller Alessandro (SSZ) <Alessandro.Feller@zuerich.ch>
2026-03-19 08:56:03 +0100

    remove icons form htmlDependency

--- R/add_zcss_deps.R
+++ R/add_zcss_deps.R
@@ -1,26 +1,21 @@
-#' Add zuericssstyle CSS and icon dependencies to an HTML tag
+#' Add zuericssstyle CSS dependencies to an HTML tag
 #'
-#' Attach the package CSS (from `inst/css/`) and the package icons
-#' (from `inst/icons/`) as html dependencies to a tag or tagList. This
-#' ensures styles and icon assets are available when rendering HTML,
+#' Attach the package CSS (from `inst/css/`) as html dependencies to a tag or tagList. This
+#' ensures styles are available when rendering HTML,
 #' pkgdown sites, htmlwidgets, or Shiny apps.
 #'
 #' @param tag A htmltools tag or tagList (for example the UI returned by
 #'   `shiny::fluidPage()`). The returned value is a `tagList` that includes
 #'   the provided `tag` and the package dependencies.
 #'
-#' @return A `tagList` containing `tag` plus the CSS and icons htmlDependencies.
+#' @return A `tagList` containing `tag` plus the CSS htmlDependencies.
 #' @export
 #'
 #' @examples
 #' \dontrun{
-#' library(shiny)
-#' ui <- add_zcss_deps(fluidPage(
-#'   tags$h1("Demo"),
-#'   tags$img(src = "icons/download.svg", alt = "download")
-#' ))
-#' shinyApp(ui, server = function(input, output) {})
+#' inst / examples / ssz_deps_with_bootstrap / app.r
 #' }
+#'
 add_zcss_deps <- function(tag) {
   shiny::tagList(
     tag,
@@ -29,13 +24,6 @@ add_zcss_deps <- function(tag) {
       packageVersion("zuericssstyle"),
       src = c(file = "css"),
       stylesheet = "ssz_shiny.min.css",
-      package = "zuericssstyle"
-    ),
-    # Expose package icons so HTML/htmlwidgets/pkgdown can resolve their paths
-    htmltools::htmlDependency(
-      "zuericssstyle-icons",
-      packageVersion("zuericssstyle"),
-      src = c(file = "icons"),
       package = "zuericssstyle"
     )
   )
