9321907aa48d5f1fb7b19e8e0d4caa0c36c35ecd
Feller Alessandro (SSZ) <Alessandro.Feller@zuerich.ch>
2026-03-25 17:43:02 +0100

    do not import icons package

--- R/zuericssstyle-package.R
+++ R/zuericssstyle-package.R
@@ -3,7 +3,6 @@
 
 ## usethis namespace: start
 #' @import here
-#' @import icons
 #' @importFrom dqshiny autocomplete_input
 #' @importFrom htmltools tags
 #' @importFrom shiny dateRangeInput selectInput textInput radioButtons actionButton numericInput downloadButton sliderInput
